a0022b899f943766123391f83d0a6a02057cdf72
Maximuele <markokots@gmail.com>
2022-12-18 15:22:58 -0800

    101 fix

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# script should parse logfile and output [sender],[receiver],[flags]
-puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+#Auth: Sangwani P Zyambo
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
